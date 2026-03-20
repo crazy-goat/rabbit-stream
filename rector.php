b05cc7bc7df1e120690224bbed008249d1ce2cc0
Piotr Hałas <piotr.h@decodo.com>
2026-03-20 09:56:01 +0100

    chore: add rector.php with DEAD_CODE, CODE_QUALITY, UP_TO_PHP_81 sets

---
+++ rector.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->withSets([
+        LevelSetList::UP_TO_PHP_81,
+        SetList::DEAD_CODE,
+        SetList::CODE_QUALITY,
+        SetList::TYPE_DECLARATION,
+        SetList::PRIVATIZATION,
+        SetList::EARLY_RETURN,
+    ]);
