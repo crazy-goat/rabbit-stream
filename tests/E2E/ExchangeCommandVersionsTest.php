9dcfc0587a2277a5bd75fa2ab3a50159f075b003
Piotr Hałas <piotr.h@decodo.com>
2026-03-21 20:47:23 +0100

    fix(tests): use TuneResponseV1 instead of TuneRequestV1 in E2E tests
    Fixes #178 - Three E2E test files were incorrectly using TuneRequestV1
    when responding to the server's tune handshake. The client should respond
    with TuneResponseV1 (key 0x8014) per the RabbitMQ Stream Protocol spec.
    
    Affected files:
    - StreamStatsTest.php
    - ExchangeCommandVersionsTest.php
    - PartitionsTest.php

--- tests/E2E/ExchangeCommandVersionsTest.php
+++ tests/E2E/ExchangeCommandVersionsTest.php
@@ -12,6 +12,7 @@ use CrazyGoat\RabbitStream\Request\SaslAuthenticateRequestV1;
 use CrazyGoat\RabbitStream\Request\SaslHandshakeRequestV1;
 use CrazyGoat\RabbitStream\Request\TuneRequestV1;
 use CrazyGoat\RabbitStream\Response\ExchangeCommandVersionsResponseV1;
+use CrazyGoat\RabbitStream\Response\TuneResponseV1;
 use CrazyGoat\RabbitStream\StreamConnection;
 use CrazyGoat\RabbitStream\VO\CommandVersion;
 use PHPUnit\Framework\TestCase;
@@ -43,7 +44,7 @@ class ExchangeCommandVersionsTest extends TestCase
 
         $tune = $connection->readMessage();
         $this->assertInstanceOf(TuneRequestV1::class, $tune);
-        $connection->sendMessage(new TuneRequestV1($tune->getFrameMax(), $tune->getHeartbeat()));
+        $connection->sendMessage(new TuneResponseV1($tune->getFrameMax(), $tune->getHeartbeat()));
 
         $connection->sendMessage(new OpenRequestV1('/'));
         $connection->readMessage();
